e9daa42c52f281040d703e2abc3611a4115c25d0
Andrew <andrewadutka@gmail.com>
2025-11-08 09:59:13 -0400

    init

---
+++ frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
+			<main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 dark:bg-black sm:items-start"></main>
+		</div>
+	);
+}
